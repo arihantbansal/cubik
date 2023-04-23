bf1ee9c85041295f6cbc2f1230d274028f4b4945
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-04-23 14:47:18 +0530

    feat: added qf API

---
+++ src/server/utils/qf.ts
@@ -0,0 +1,55 @@
+import { Prisma } from '@prisma/client';
+
+type RoundAllType = Prisma.RoundGetPayload<{
+  include: {
+    Contribution: {
+      include: {
+        ProjectsModel: true;
+      };
+    };
+    ProjectJoinRound: true;
+  };
+}>;
+
+export const qfV1 = (round: RoundAllType) => {
+  const matchingPool = round.matchedPool;
+  let summed = 0;
+  const arrOfMatch: {
+    projectId: string;
+    sum: number;
+  }[] = [];
+  const projectMapContribution = round.Contribution.map((contribution) => {
+    return {
+      projectId: contribution.projectId,
+      amount: contribution.usdContribution,
+    };
+  });
+
+  round.ProjectJoinRound.forEach((projectJoin) => {
+    let sumAmount = 0;
+    projectMapContribution
+      .filter((project) => project.projectId === projectJoin.projectId)
+      .forEach((project) => {
+        sumAmount += Math.sqrt(project.amount);
+      });
+
+    sumAmount *= sumAmount;
+    summed += sumAmount;
+    arrOfMatch.push({
+      projectId: projectJoin.projectId,
+      sum: sumAmount,
+    });
+  });
+  let divisor = matchingPool / summed;
+
+  const finalMatch = round.ProjectJoinRound.map((project) => {
+    return {
+      projectId: project.projectId,
+      amount:
+        arrOfMatch.filter((e) => e.projectId === project.projectId)[0].sum *
+        divisor,
+    };
+  });
+
+  return finalMatch;
+};
