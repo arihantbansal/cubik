e2490bda073521ebcee841c0a7dcd13b3b79389b
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-05-23 16:25:29 +0530

    change config

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,9 +2,9 @@ import * as trpcNext from '@trpc/server/adapters/next';
 import { createContext } from '~/server/createContext';
 import { appRouter } from '../../../server/routers/_app';
 
-export const config = {
-  runtime: 'edge',
-};
+// export const config = {
+//   runtime: 'edge',
+// };
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
