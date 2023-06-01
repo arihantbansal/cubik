d85b9a124b08f950df8974912f00d1e27430b7f8
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-06-01 13:43:40 +0530

    fix: grant mutation

--- src/pages/grants/new-grant.tsx
+++ src/pages/grants/new-grant.tsx
@@ -18,22 +18,26 @@ import {
   useToast,
   VStack,
 } from '@chakra-ui/react';
+import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useAnchorWallet } from '@solana/wallet-adapter-react';
 import { addDays } from 'date-fns';
 import enGB from 'date-fns/locale/en-GB';
+import moment from 'moment';
+import { getSession, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useState } from 'react';
 import { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { useForm } from 'react-hook-form';
+import { useForm, UseFormGetValues } from 'react-hook-form';
 import { BiChevronLeft } from 'react-icons/bi';
 import * as yup from 'yup';
 import GrantStepOne from '~/components/pages/grants/create-grant/GrantStepOne';
 import GrantStepTwo from '~/components/pages/grants/create-grant/GrantStepTwo';
 import GrantStepZero from '~/components/pages/grants/create-grant/GrantStepZero';
-import { onSignTransaction } from '~/utils/round/createGrantsRound';
+import { connection, createRoundIx } from '~/utils/program/contract';
 import { trpc } from '~/utils/trpc';
+import * as anchor from '@coral-xyz/anchor';
 
 registerLocale('en-gb', enGB);
 
@@ -121,7 +125,8 @@ const CreateGrantRound = () => {
   const [endDate, setEndDate] = useState(tomorrow);
   const [startDate, setStartDate] = useState(tomorrow);
   const [editorData, setEditorData] = useState();
-
+  const createRoundMutation = trpc.round.create.useMutation();
+  const { data: session } = useSession();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [transactionError, setTransactionError] = useState<string | null>(null);
@@ -147,6 +152,87 @@ const CreateGrantRound = () => {
     }
   };
 
+  const createRound = async (
+    anchorWallet: any, // Add the specific type here
+    name: string,
+    pool: string,
+    project: string,
+    colorScheme: string,
+    description: string,
+    start: moment.Moment | null,
+    end: moment.Moment | null,
+    onClose: () => void,
+    setTransactionError: (error: string | null) => void
+  ) => {
+    try {
+      const ix = await createRoundIx(
+        anchorWallet as NodeWallet,
+        name,
+        parseInt(pool),
+        parseInt(project)
+      );
+      const { blockhash } = await connection.getLatestBlockhash();
+      const tx = new anchor.web3.Transaction();
+      tx.recentBlockhash = blockhash;
+      tx.feePayer = anchorWallet?.publicKey;
+      tx.add(ix as anchor.web3.TransactionInstruction);
+      const signed = await anchorWallet?.signTransaction(tx);
+      const txid = await connection.sendRawTransaction(signed!.serialize());
+      if (!txid) {
+        throw new Error('txid is null');
+      }
+      createRoundMutation.mutate({
+        matchingPool: parseInt(pool),
+        name: name,
+        notionPage: 'https://www.notion.so/round1',
+        projectCount: parseInt(project),
+        tx: txid,
+        colorScheme: colorScheme,
+        short_description: description,
+        startTime: start?.toISOString() as string,
+        endtime: end?.toISOString() as string,
+        description: description,
+        manager: (session?.user.username as string) ?? '',
+      });
+      onClose();
+    } catch (error: any) {
+      setTransactionError(error.message || 'Error while signing transaction');
+    }
+  };
+
+  const onSignTransaction = async (
+    startDate: Date,
+    endDate: Date,
+    description: string | undefined,
+    getValues: UseFormGetValues<FormData>,
+    setSignTransactionLoading: (_loading: boolean) => void,
+    setTransactionError: (_error: string | null) => void,
+    anchorWallet: any, // Add the specific type here
+    onClose: () => void
+  ) => {
+    setSignTransactionLoading(true);
+    try {
+      const formValues = getValues(); // get the form values here
+      const startMoment = moment(startDate);
+      const endMoment = moment(endDate);
+      createRound(
+        anchorWallet,
+        formValues.name,
+        formValues.pool,
+        formValues.projects,
+        formValues.colorScheme,
+        formValues.short_description,
+        startMoment,
+        endMoment,
+        onClose,
+        setTransactionError
+      );
+    } catch (error: any) {
+      setTransactionError(error.message || 'there was an error');
+    } finally {
+      setSignTransactionLoading(false);
+    }
+  };
   return (
     <Container
       transition="all .25s ease"
