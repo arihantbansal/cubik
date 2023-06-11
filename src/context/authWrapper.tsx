79dcf2ac3b0d02791e6d2715b4826d7cdd641624
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-06-11 17:25:36 +0530

    remove old auth

--- src/context/authWrapper.tsx
+++ src/context/authWrapper.tsx
@@ -17,62 +17,6 @@ export const AuthWrapper: React.FC<Props> = ({ children }) => {
   const { publicKey, connected } = useWallet();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // const checkAndVerifySignature = async () => {
-  //   const signatureStore = getSignature();
-  //   console.log('verifying wallet -2');
-  //   if (!publicKey) {
-  //     return;
-  //   }
-  //   // if (!connected && session?.user) {
-  //   //   return signOut({
-  //   //     redirect: false,
-  //   //   });
-  //   // }
-
-  //   if (!signatureStore) {
-  //     // check for available signature
-  //     onOpen();
-  //     return;
-  //   }
-
-  //   if (signatureStore && signatureStore.wallet !== publicKey.toBase58()) {
-  //     // check for available signature for current wallet
-  //     onOpen();
-  //     return;
-  //   }
-  //   try {
-  //     // Try Login
-  //     const signInResponse = await signIn('credentials', {
-  //       redirect: false,
-  //       wallet: publicKey?.toBase58(),
-  //       signature: signatureStore?.signature,
-  //     });
-
-  //     if (signInResponse?.status === 401) {
-  //       console.log('redirecting to create profile');
-  //       if (session?.user.id) {
-  //         console.log('redirecting to ss');
-  //         await signOut({
-  //           redirect: false,
-  //         });
-  //         setKey({
-  //           sig: '',
-  //           wallet: '',
-  //         });
-  //       }
-  //       console.log('redirecting to create profile');
-
-  //       router.push('/create-profile');
-  //       return;
-  //     }
-
-  //     if (signInResponse?.error) {
-  //       return;
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
   const checkAndVerifySignature = () => {
     if (!publicKey || !connected) {
       return;
