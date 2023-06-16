868b1a9d1dae535c5479d4bcc9eec8f2e11f311a
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-06-16 21:30:37 +0530

    feat: branch

---
+++ src/hooks/useCurrentTokenPrice.tsx
@@ -0,0 +1,10 @@
+import { useQuery } from 'react-query';
+import { fetchPrice } from '~/utils/getPrice';
+
+const useCurrentTokenPrice = (type: 'solana' | 'usd' | 'bonk') => {
+  return useQuery<number | null, Error>(['price', type], () =>
+    fetchPrice(type)
+  );
+};
+
+export default useCurrentTokenPrice;
