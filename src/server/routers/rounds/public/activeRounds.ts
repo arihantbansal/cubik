995aa16bc15acf7b7d88177ef95ab85005dfe48a
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-06-24 16:52:03 +0530

    put logs on error

--- src/server/routers/rounds/public/activeRounds.ts
+++ src/server/routers/rounds/public/activeRounds.ts
@@ -2,13 +2,19 @@ import { publicProcedure } from '~/server/trpc';
 import { prisma } from '~/server/utils/prisma';
 
 export const activeRounds = publicProcedure.query(async ({ ctx }) => {
-  const res = await prisma.round.findMany({
-    where: {
-      active: true,
-      endTime: {
-        gte: new Date(),
+  try {
+    const res = await prisma.round.findMany({
+      where: {
+        active: true,
+        endTime: {
+          gte: new Date(),
+        },
       },
-    },
-  });
-  return res;
+    });
+    return res;
+  } catch (error) {
+    console.log(error);
+
+    throw new Error('Error fetching active rounds');
+  }
 });
