a3b67fddb61aae0b90523acbc5999367cba0d6ee
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-06-26 19:22:54 +0530

    fix: drip proofs

--- src/components/pages/user-profile/details-tab/proofs/DripProof.tsx
+++ src/components/pages/user-profile/details-tab/proofs/DripProof.tsx
@@ -10,10 +10,13 @@ import {
   Tag,
   Text,
   useDisclosure,
+  useToast,
   VStack,
 } from '@chakra-ui/react';
+import { Player } from '@lottiefiles/react-lottie-player';
 import axios from 'axios';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { SuccessToast } from '~/components/common/toasts/Toasts';
 import { useUserStore } from '~/store/userStore';
 import { trpc } from '~/utils/trpc';
 import DripHauz from './SVGs/DripHauz';
@@ -24,9 +27,27 @@ interface Props {
 const DripProof = ({ claimed }: Props) => {
   const { user } = useUserStore();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [canClaim, setCanClaimed] = useState<boolean>(false);
+  const [canClaim, setCanClaimed] = useState<boolean>(true);
   const [error, setError] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+  const playerRef = useRef<Player>(null);
+  const toast = useToast();
+  const utils = trpc.useContext();
+  const handleDripProof = trpc.user.dripProof.useMutation({
+    onSuccess: () => {
+      playerRef?.current?.play();
+      utils.user.findOne.invalidate({
+        username: user?.username,
+      });
+      setTimeout(() => {
+        onClose();
+        SuccessToast({
+          toast,
+          message: 'Proof minted successfully',
+        });
+      }, 2000);
+    },
+  });
 
   const handleClaim = async () => {
     try {
@@ -38,6 +59,7 @@ const DripProof = ({ claimed }: Props) => {
       console.log(res.data);
       if (res.data.data > 0) {
         setCanClaimed(true);
+        handleDripProof.mutate();
       } else {
         setError("You don't have any drip s1 NFTs");
         setCanClaimed(false);
@@ -50,13 +72,7 @@ const DripProof = ({ claimed }: Props) => {
       setLoading(false);
     }
   };
-  const handleDripProof = trpc.user.dripProof.useMutation({
-    onSuccess: () => {
-      console.log('success');
-      /// add confettie and everything here
-      onClose();
-    },
-  });
+
   return (
     <>
       <VStack onClick={onOpen} p="32px" gap="8px" align="start">
@@ -119,6 +135,23 @@ const DripProof = ({ claimed }: Props) => {
               >
                 <VStack spacing="24px">
                   <Center transform={'scale(2)'} h="130px">
+                    <Center
+                      h="0"
+                      overflow="visible"
+                      top="0px"
+                      position="absolute"
+                    >
+                      <Player
+                        ref={playerRef}
+                        autoplay={false}
+                        controls={true}
+                        speed={0.7}
+                        src={
+                          'https://assets4.lottiefiles.com/packages/lf20_obhph3sh.json'
+                        }
+                        style={{ height: `300px`, width: `300px` }}
+                      />
+                    </Center>
                     <DripHauz size={'60px'} />
                   </Center>
                   <VStack spacing="12px">
@@ -148,36 +181,29 @@ const DripProof = ({ claimed }: Props) => {
                   </>
                 ) : canClaim ? (
                   <>
-                    {handleDripProof.isError && (
-                      <>
-                        <Text>{handleDripProof.error.message}</Text>
-                      </>
-                    )}
-                    <Button
-                      onClick={() => {
-                        handleDripProof.mutate();
-                      }}
-                      variant={'connect_wallet'}
-                      w="12rem"
-                    >
-                      Claim
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    {error && (
-                      <>
-                        <Text>{error}</Text>
-                      </>
-                    )}
                     <Button
                       onClick={handleClaim}
                       variant={'connect_wallet'}
                       w="12rem"
                       isLoading={loading}
                     >
-                      Verify Wallet
+                      Claim
                     </Button>
+                  </>
+                ) : (
+                  <>
+                    {handleDripProof.isError && (
+                      <>
+                        {!error && handleDripProof.error && (
+                          <Text>{handleDripProof.error.message}</Text>
+                        )}
+                        {error && (
+                          <>
+                            <Text>{error}</Text>
+                          </>
+                        )}
+                      </>
+                    )}
                   </>
                 )}
               </VStack>
