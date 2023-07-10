9a880801ffe1da4c22e5264a34edd52b3f07606a
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-07-10 19:36:06 +0530

    fix: trpc routers

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,6 +1,22 @@
 import { createTRPCRouter } from './trpc';
-
-export const appRouter = createTRPCRouter({});
+import {
+  projectRouter,
+  userRouter,
+  contributionRouter,
+  hackathonRouter,
+  roundRouter,
+  poolRouter,
+  commentRouter,
+} from './router';
+export const appRouter = createTRPCRouter({
+  user: userRouter,
+  project: projectRouter,
+  contribution: contributionRouter,
+  hackathon: hackathonRouter,
+  round: roundRouter,
+  pool: poolRouter,
+  comment: commentRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- packages/api/src/router/pool/index.ts
+++ packages/api/src/router/pool/index.ts
@@ -1,3 +1,3 @@
 import { createTRPCRouter } from './../../trpc';
 
-export const userRouter = createTRPCRouter({});
+export const poolRouter = createTRPCRouter({});
