438034f367c3698c8eac22459cef4ad9f6348db0
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-07-16 15:25:27 +0530

    feat: added context

--- apps/cubik-grants/src/pages/_app.tsx
+++ apps/cubik-grants/src/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { AppProps, AppType } from 'next/app';
+import dynamic from 'next/dynamic';
+import { QueryClient, QueryClientProvider } from 'react-query';
+// import AppLayout from '~/components/app';
+import theme from '~/config/chakra.config';
+// import { Mixpanel } from '~/utils/mixpanel';
+import { trpc } from '../utils/trpc';
+import { ChakraProvider } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const WalletContext: any = dynamic(() => import('../context/wallet-context'), {
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+const MyApp: AppType = ({
+  Component,
+  pageProps: { ...pageProps },
+}: AppProps) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <WalletContext>
+          <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </WalletContext>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
