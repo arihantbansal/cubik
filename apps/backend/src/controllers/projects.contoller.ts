cc3fdef41ba0dceb9ef3c244aaad997395a48101
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-07-27 16:08:30 +0530

    update api

--- apps/backend/src/controllers/projects.contoller.ts
+++ apps/backend/src/controllers/projects.contoller.ts
@@ -1,13 +1,57 @@
-import {} from '@cubik/comman-types';
-import { dbInit } from '../service/pscale';
+import { ProjectExplorerType } from '@cubik/comman-types';
+import { prisma } from '@cubik/database';
 import { Request, Response } from 'express';
 
 export const projectExplorer = async (req: Request, res: Response) => {
   try {
-    const pool = dbInit();
-    const [row] = await pool.promise().query('SELECT * FROM ProjectsModel');
-
-    return res.status(200).json({ row });
+    const projectJoinRound = await prisma.projectJoinRound.findMany({
+      where: {
+        status: 'APPROVED',
+        fundingRound: {
+          endTime: {
+            gte: new Date(),
+          },
+        },
+        isArchive: false,
+      },
+      select: {
+        id: true,
+        _count: true,
+        amountRaise: true,
+        fundingRound: {
+          select: {
+            roundName: true,
+            colorScheme: true,
+            matchedPool: true,
+          },
+        },
+        project: {
+          select: {
+            owner: {
+              select: {
+                username: true,
+              },
+            },
+            Contribution: {
+              select: {
+                user: {
+                  select: {
+                    profilePicture: true,
+                  },
+                },
+              },
+            },
+            logo: true,
+            short_description: true,
+            industry: true,
+          },
+        },
+      },
+    });
+    // const projectJoinHackathon = await prisma.projectJoinHackathons.findMany({
+    //   where: {},
+    // });
+    return res.status(200).json(projectJoinRound);
   } catch (error) {
     console.log(error);
     return res.status(200).json({ error });
--- apps/backend/src/service/pscale.ts
+++ apps/backend/src/service/pscale.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 config();
 export const dbInit = () => {
   const pool: Pool = mysql.createPool({
-    uri: 'mysql://ydbq6oqssbpiq5nj63bz:pscale_pw_2yi2xQiCYLOnVWWI8J3vH3eswfrDObbznvMIpr4SkHA@aws.connect.psdb.cloud/cubik?ssl={"rejectUnauthorized":true}',
+    uri: process.env.DATABASE_URL,
     connectionLimit: 10,
   });
   logger.info(`Connected to Planetscale`);
