68906bab2b571dadc0793e259232c023daaecc7d
Arpita Ganata <ganatraarpita@gmail.com>
2023-10-17 20:43:54 +0530

    custom svg icon component (#315)
    * custom svg icon component
    
    * added more props to icon component

---
+++ packages/ui/icons/icon.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { iconLibrary } from "./iconLibrary";
+import clsx from "clsx";
+
+type IconName = keyof typeof iconLibrary;
+
+type Props = {
+  name: IconName;
+  height?: number;
+  width?: number;
+  strokeWidth?: number;
+  fill?: string;
+  stroke?: string;
+  className?: string;
+};
+export const Icon = ({
+  name,
+  height = 24,
+  width = 24,
+  strokeWidth = 2,
+  fill = "#fff",
+  stroke = "#CCCCCC",
+  className,
+}: Props) => {
+  const renderedPaths = iconLibrary[name]?.paths.map(
+    (path: string, index: number) => <path key={index} d={path}></path>
+  );
+
+  const viewBox = iconLibrary[name]?.viewBox;
+
+  return (
+    <svg
+      viewBox={viewBox}
+      className={clsx(" text-white", className)}
+      stroke-width={strokeWidth}
+      fill={fill}
+      stroke={stroke}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      width={width}
+      height={height}
+    >
+      {renderedPaths}
+    </svg>
+  );
+};
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,3 +3,4 @@ export * from "./components/ui/text";
 export * from "./components/ui/tabs";
 export * from "./components/ui/table";
 export * from "./components/ui/input";
+export * from "./icons/icon";
