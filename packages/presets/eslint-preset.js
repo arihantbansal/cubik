f843d646ddd37566a3eec39fbe914684eb488ca3
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-30 13:30:25 +0530

    feat: eslint

--- packages/presets/eslint-preset.js
+++ packages/presets/eslint-preset.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'tailwindcss/no-custom-classname': 'off',
   },
   settings: {
     next: {
