0520aaab4ac142f3adc74f432f6a00e96e7ff5cb
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-31 15:23:04 +0530

    fix: build

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,8 +1,6 @@
-import { connect } from '@planetscale/database';
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
+/* eslint-disable no-var */
 import { PrismaClient } from '@prisma/client';
 import { config } from 'dotenv';
-import { fetch as undiciFetch } from 'undici';
 
 export * from '@prisma/client';
 
@@ -14,17 +12,11 @@ declare global {
 let prisma: PrismaClient;
 
 if (typeof window === 'undefined') {
-  const connection = connect({
-    url: process.env.PROD_DATABASE_URL,
-    fetch: undiciFetch,
-  });
-  const adapter = new PrismaPlanetScale(connection);
-
   if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient({ adapter });
+    prisma = new PrismaClient();
   } else {
     if (!global.prisma) {
-      global.prisma = new PrismaClient({ adapter });
+      global.prisma = new PrismaClient();
     }
     prisma = global.prisma;
   }
