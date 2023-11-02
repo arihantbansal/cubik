ec4028a233450bc992f8931f09ee37bced3ee786
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-11-02 14:53:40 +0530

    feat: added input components

--- packages/ui/components/ui/Input/input.tsx
+++ packages/ui/components/ui/Input/input.tsx
@@ -1,31 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { cn } from '../../../lib/utils';
 
 interface Props {
   type: React.HTMLInputTypeAttribute;
   name: string;
   id: string;
   placeholder: string;
-  leftIcon: React.ReactNode;
+  leftElement?: React.ReactNode;
+  rightElement?: React.ReactNode;
+  isHttps?: boolean;
+  isError?: boolean;
+  size?: 'md' | 'sm';
+  isDisabled?: boolean;
 }
-export const Input = ({ type, id, name, placeholder, leftIcon }: Props) => {
+export const Input = ({
+  type,
+  id,
+  name,
+  placeholder,
+  isHttps = true,
+  isError = false,
+  size = 'md',
+  leftElement,
+  rightElement,
+  isDisabled,
+}: Props) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   return (
     <>
-      <div className="mt-2 flex rounded-[8px] shadow-sm">
-        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
-          {leftIcon}
-        </span>
+      <div
+        className={cn(
+          isDisabled && 'cursor-not-allowed',
+          size === 'md' ? 'h-[40px]' : 'h-[36px]',
+          'mt-2 flex rounded-[8px]',
+          isError
+            ? 'border-[var(--form-input-border-error)] bg-[var(--form-input-surface-error)] text-[var(--form-input-fg-error)]'
+            : 'border-[var(--form-input-border-default)]',
+          isFocused &&
+            'border-[var(--form-input-border-focused)] bg-[var(--form-input-surface-focused)] text-[var(--form-input-fg-focused)]',
+          isDisabled &&
+            'border-[var(--form-input-border-disabled)] bg-[var(--form-input-surface-disabled)',
+          'hover:text-[var(--form-input-fg-hovered)] border-2  hover:border-[var(--form-input-border-hovered)] hover:bg-[var(--form-input-surface-hovered)',
+        )}
+      >
+        {leftElement && (
+          <div
+            className={cn(
+              isError && !isFocused
+                ? 'border-[var(--form-input-border-error)] bg-[var(--form-input-surface-error)] text-[var(--form-input-fg-error)]'
+                : 'border-[var(--form-input-border-default)] bg-[var(--form-input-surface-default)] text-[var(--form-input-fg-default)]',
+              isFocused &&
+                'border-[var(--form-input-border-focused)] bg-[var(--form-input-surface-focused)]',
+              isDisabled &&
+                'text-[var(--form-input-fg-disabled)] cursor-not-allowed  border-[var(--form-input-border-disabled)] bg-[var(--form-input-surface-disabled)',
+              !leftElement
+                ? 'hidden'
+                : 'inline-flex items-center rounded-l-[8px] px-3 text-[var(--form-input-fg-default)]',
+              isHttps &&
+                'border-r-1 border border-[var(--form-input-border-default)]',
+            )}
+          >
+            {leftElement}
+          </div>
+        )}
         <input
           type={type}
           name={name}
           id={id}
+          disabled={isDisabled}
           placeholder={placeholder}
-          className="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:px-2 placeholder:text-[var(--form-input-border-default)] focus:ring-2 focus:ring-inset focus:ring-[var(--form-input-border-focused)] sm:text-sm sm:leading-6"
+          onFocus={() => !isError && setIsFocused(true)}
+          onBlur={() => !isError && setIsFocused(false)}
+          value={JSON.stringify(isError)}
+          className={cn(
+            !isFocused &&
+              'border-[var(--form-input-border-default)] bg-[var(--form-input-surface-default)] text-[var(--form-input-fg-default)]',
+            isFocused &&
+              'focus:border-[var(--form-input-border-focused)] bg-[var(--form-input-surface-focused)]',
+            'disabled:text-[var(--form-input-fg-disabled)] disabled:cursor-not-allowed disabled:opacity-50 disabled:border-[var(--form-input-border-disabled)] disabled:bg-[var(--form-input-surface-disabled)',
+            'block w-full flex-1 px-2 rounded-[8px] border-0 py-1.5 placeholder:px-2 placeholder:text-[var(--form-input-border-default)] outline-none',
+          )}
         />
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-          <span className="text-gray-500 sm:text-sm" id="price-currency">
-            USD
-          </span>
-        </div>
+        {rightElement && (
+          <div
+            className={cn(
+              isError && !isFocused
+                ? 'border-[var(--form-input-border-error)]  bg-[var(--form-input-surface-error)] text-[var(--form-input-fg-error)]'
+                : 'border-[var(--form-input-border-default)] bg-[var(--form-input-surface-default)] text-[var(--form-input-fg-default)]',
+              isFocused &&
+                'border-[var(--form-input-border-focused)] bg-[var(--form-input-surface-focused)]',
+              isDisabled &&
+                'text-[var(--form-input-fg-disabled)] cursor-not-allowed  border-[var(--form-input-border-disabled)] bg-[var(--form-input-surface-disabled)',
+              !rightElement
+                ? 'hidden'
+                : 'relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-[8px] px-3 py-2 text-sm font-semibold border-0 border-l-0',
+            )}
+          >
+            {rightElement}
+          </div>
+        )}
       </div>
     </>
   );
