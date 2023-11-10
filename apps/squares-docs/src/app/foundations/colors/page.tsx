b9d67cebad7efc1c02557c858dd3b81692c718f4
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-11-10 11:21:55 +0530

    fix: build

--- apps/squares-docs/src/app/foundations/colors/page.tsx
+++ apps/squares-docs/src/app/foundations/colors/page.tsx
@@ -18,7 +18,6 @@ import {
   Tabs,
 } from '@cubik/ui';
 
-import { serializeSemantic } from '@cubik/helper-scripts/src/color/serializeSemantic';
 import PageHOC from '../../home-page-components/components/pageHOC';
 
 type objectType = {
